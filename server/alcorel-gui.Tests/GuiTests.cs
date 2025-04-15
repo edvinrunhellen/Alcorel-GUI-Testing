b817595d9e427ca059e8f690cb13c931bafefbbc
Eddan <edvin.runhellen1@gmail.com>
2025-04-15 16:27:32 +0200

    Desperately trying to fix 1 test, manageticketascustomer

--- server/alcorel-gui.Tests/GuiTests.cs
+++ server/alcorel-gui.Tests/GuiTests.cs
@@ -144,7 +144,8 @@ public class GuiTest : PageTest
     {
         _page.SetDefaultTimeout(60000);
         await _page.GotoAsync("http://localhost:5001/customer-view/98353a155b5346449f49b0acb9a28b38");
-        await _page.Locator("input[placeholder*='reply' i], textarea[placeholder*='reply' i], input[name*='reply' i], textarea[name*='reply' i]").First.FillAsync("alright, thanks for the help");
+        await _page.GetByRole(AriaRole.Textbox, new() { Name = "Reply..." }).ClickAsync();
+        await _page.GetByRole(AriaRole.Textbox, new() { Name = "Reply..." }).FillAsync("alright");
         await _page.GetByRole(AriaRole.Button, new() { Name = "Send Reply" }).ClickAsync();
 
     }
